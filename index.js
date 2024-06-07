bcd93a41c9754bb3d4e99e6123e9f0f7966eb9fb
Константин <kaptor2@gmail.com>
2024-06-07 06:17:03 +0400

    asd

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 setTimeout(() => {
     document.querySelector('#video').play();
 }, 100)
+
+AFRAME.registerComponent("listener", {
+    init: function () {
+        this.target = document.querySelector('#video');
+        this.prevPosition = null;
+        this.prevRotation = null;
+    },
+
+    tick: function () {
+        this.target.setAttribute('visible', 'true')
+
+        if (!this.prevPosition && !this.prevRotation) {
+            this.target.setAttribute('position', this.el.getAttribute('position'))
+            this.target.setAttribute('rotation', this.el.getAttribute('rotation'))
+        } else {
+            this.target.object3D.position.lerp(this.prevPosition, 0.1)
+            let rot = this.target.object3D.rotation.toVector3().lerp(this.prevRotation, 0.1)
+            this.target.object3D.rotation.setFromVector3(rot)
+        }
+
+        this.prevPosition = this.el.object3D.position
+        this.prevRotation = this.el.object3D.rotation
+
+    }
+})
