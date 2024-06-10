46983c019821a064797d78fa2931379bd8a5e818
Константин <kaptor2@gmail.com>
2024-06-10 22:15:19 +0400

    asd

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ document.querySelector('#lounch').addEventListener('click', () => {
     document.querySelector('#video').play();
     document.querySelector('[spatial-button]').click();
     document.querySelector('#louncher').style.display = "none"
-    document.addEventListener('click', () => {
-        console.log(AFRAME.modalVisible)
+    document.addEventListener('touchstart', () => {
+
         if (AFRAME.modalVisible) {
             document.querySelector('[spatial-close-button]').click();
         }
