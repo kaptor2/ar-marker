27d044b3b6a2e9e94b9fed454fe969b5a911aad8
Константин <kaptor2@gmail.com>
2024-06-10 22:43:05 +0400

    asd

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 document.querySelector('#lounch').addEventListener('click', () => {
     document.querySelector('#video').play();
     document.querySelector('[spatial-button]').click();
-    document.querySelector('#louncher').remove();
+    document.querySelector('#louncher').style.display = 'none';
     document.addEventListener('click', () => {
 
         if (AFRAME.modalVisible) {
