dbb1be3bd4c8ece42a06cc829417ac1b87db2994
Константин <kaptor2@gmail.com>
2024-06-10 22:34:33 +0400

    asd

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
-
-
-
 document.querySelector('#lounch').addEventListener('click', () => {
-    document.querySelector('look-controls').removeEventListener('touchmove');
     document.querySelector('#video').play();
-    document.querySelector('[spatial-button]').dispatchEvent(new TouchEvent('click', {
-        bubbles: true,
-        cancelable: true,
-    }));
+    document.querySelector('[spatial-button]').click();
     document.querySelector('#louncher').remove();
     document.addEventListener('click', () => {
 
         if (AFRAME.modalVisible) {
-            document.querySelector('[spatial-close-button]').dispatchEvent(new TouchEvent('click', {
-                bubbles: true,
-                cancelable: true,
-            }));
+            document.querySelector('[spatial-close-button]').click()
         }
 
     })
