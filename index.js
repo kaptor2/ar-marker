611dad07db8e2174fd2da121094802fb049410d1
Константин <kaptor2@gmail.com>
2024-06-10 21:28:16 +0400

    asd

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-setTimeout(() => {
+document.querySelector('#lounch').addEventListener('click', () => {
     document.querySelector('#video').play();
-}, 1000)
+    document.querySelector('[spatial-button]').click();
+    document.querySelector('#louncher').style.display = "none"
+
+})
